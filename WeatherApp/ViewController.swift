d802b79f50ee80f8a337bf54bfcad785b7c95f5a
Prateek Bhatt <prattbhatt@gmail.com>
2015-01-15 18:47:29 +0530

    added functionality to fetch weather of a city from the internet and display the summary; also displays an error if the city was not found

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -7,12 +7,54 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var city: UITextField!
+    
+    @IBOutlet weak var message: UILabel!
+    
+    @IBAction func findWeatherButton(sender: AnyObject) {
+        
+        self.view.endEditing(true)
+        
+        var urlString = "http://weather-forecast.com/locations/" + city.text.stringByReplacingOccurrencesOfString(" ", withString: "") + "/forecasts/latest"
+        var url = NSURL(string: urlString)
+        
+        println(urlString)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!){(data, response, error) in
+            
+            var webpageContent = NSString(data: data, encoding: NSUTF8StringEncoding)
+            
+            let tempwebpageContent: String = webpageContent as String
+            
+            println(tempwebpageContent)
+            if (tempwebpageContent.rangeOfString("<span class=\"phrase\">") != nil) {
+                
+                var contentArray = webpageContent?.componentsSeparatedByString("<span class=\"phrase\">")
+            
+                var messageText: AnyObject? = contentArray?[1].componentsSeparatedByString("</span>")[0] as String
+                println(messageText)
+            
+                self.message.text = messageText?.stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                
+            } else {
+                
+                self.message.text = "Couldn't find that city - please try again"
+            }
+        }
+        
+        task.resume()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.message.text = ""
     }
 
     override func didReceiveMemoryWarning() {
